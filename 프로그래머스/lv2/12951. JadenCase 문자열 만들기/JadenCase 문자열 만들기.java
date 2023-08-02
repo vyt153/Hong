63e8efc9d28b405bfb0f1b7b3c9921661a9aaa27
vyt153 <92364685+vyt153@users.noreply.github.com>
2023-08-02 15:56:03 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 12.66 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,46 @@
+import java.util.regex.Pattern;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] words = s.split(" ");
+        
+        for(int i=0;i<words.length; i++){
+            
+            String word = words[i].trim();
+            if(word.equals("")||word==""){
+                answer += " ";
+                continue;
+            }
+            
+            if(!isInt(String.valueOf(word.charAt(0)))){  
+                
+                String upperWord = word.substring(0, 1);
+                upperWord = upperWord.toUpperCase();
+                
+                String remainWord = word.substring(1);
+                remainWord = remainWord.toLowerCase();
+                
+                answer += upperWord + remainWord;
+            } else{
+                answer += word.toLowerCase();
+            }
+            
+            if(i<words.length-1){
+                answer += " ";
+            }
+        }
+        if(s.substring(s.length() - 1).equals(" ")){
+            answer += " ";
+        }
+        return answer;
+    }
+    
+    public boolean isInt(String word){
+        try {
+          Integer.parseInt(word);
+          return true;
+        } catch (NumberFormatException ex) {
+          return false;
+        }
+    }
+}
