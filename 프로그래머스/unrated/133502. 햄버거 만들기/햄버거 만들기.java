1d669735ec399b5846679cd4e09a05fd529acc42
vyt153 <92364685+vyt153@users.noreply.github.com>
2023-08-03 12:33:57 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.50 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        
+        Stack<Integer> hamburger = new Stack<>();
+        
+        int i = 0;
+        int wrapBurger = 0;
+        
+        while(i<ingredient.length){
+            hamburger.push(ingredient[i]);
+            
+            if(hamburger.peek()-1==wrapBurger){
+                wrapBurger++;
+            } else if(hamburger.peek()==1&&wrapBurger==3){
+                hamburger.pop();hamburger.pop();
+                hamburger.pop();hamburger.pop();
+                System.out.println(1);
+                
+                answer++;
+                if(hamburger.isEmpty()){
+                    wrapBurger = 0;
+                }else{
+                    int bread = hamburger.search(1);
+                    int vegetable = hamburger.search(2);
+
+                    if(bread==1) { wrapBurger=1;}
+                    else if(bread+vegetable==3){ wrapBurger=2;}
+                    else{wrapBurger=0;}
+                }
+            } else{
+                if(hamburger.peek()==1){
+                    wrapBurger = 1;
+                }else{
+                    wrapBurger = 0;    
+                }
+                
+            }
+            i++;
+        }
+        
+        return answer;
+    }
+}
