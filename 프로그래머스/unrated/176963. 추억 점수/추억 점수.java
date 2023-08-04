7d738f9549f112541859263e20854c9ca27d61ed
vyt153 <92364685+vyt153@users.noreply.github.com>
2023-08-04 11:24:30 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        
         int[] answer = new int[photo.length];
         Map<String, Integer> map = new HashMap<>();
         
@@ -11,9 +12,7 @@ class Solution {
         
         for (int i = 0; i < photo.length; i++) {
 		    for (int j = 0; j < photo[i].length; j++) {
-			    if(map.containsKey(photo[i][j])){
-                    answer[i] += map.get(photo[i][j]);
-                }
+			    answer[i] += map.containsKey(photo[i][j]) ? map.get(photo[i][j]):0;
 		    }	
 		}
 		
