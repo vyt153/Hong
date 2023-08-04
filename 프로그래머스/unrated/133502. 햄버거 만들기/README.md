07fe67222aa7ce800cbbff16e7ca6534c7d3b895
vyt153 <92364685+vyt153@users.noreply.github.com>
2023-08-04 12:08:00 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.20 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/README.md
+++ 프로그래머스/unrated/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#qna) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 0.50 ms
+메모리: 78.6 MB, 시간: 0.20 ms
 
 ### 구분
 
--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -16,7 +16,6 @@ class Solution {
             } else if(hamburger.peek()==1&&wrapBurger==3){
                 hamburger.pop();hamburger.pop();
                 hamburger.pop();hamburger.pop();
-                System.out.println(1);
                 
                 answer++;
                 if(hamburger.isEmpty()){
@@ -25,17 +24,13 @@ class Solution {
                     int bread = hamburger.search(1);
                     int vegetable = hamburger.search(2);
 
-                    if(bread==1) { wrapBurger=1;}
-                    else if(bread+vegetable==3){ wrapBurger=2;}
-                    else{wrapBurger=0;}
+                    if(bread==1) wrapBurger=1;
+                    else if(bread+vegetable==3) wrapBurger=2;
+                    else wrapBurger=0;
                 }
             } else{
-                if(hamburger.peek()==1){
-                    wrapBurger = 1;
-                }else{
-                    wrapBurger = 0;    
-                }
-                
+                if(hamburger.peek()==1) wrapBurger = 1;
+                else wrapBurger = 0;   
             }
             i++;
         }
