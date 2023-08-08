065796b67c712c7e40caf534e3e84c87583f5547
vyt153 <92364685+vyt153@users.noreply.github.com>
2023-08-08 12:05:31 +0900

    [unrated] Title: 귤 고르기, Time: 77.08 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -2,18 +2,18 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        
+
         Map<Integer, Integer> map = new HashMap<>();
         for (int key : tangerine) {
-			map.put(key, map.getOrDefault(key, 0) + 1);
-		}
-        
-        List<Integer> sortedCounts = new ArrayList<>();
-        
-        for(Integer key : map.keySet()) sortedCounts.add(map.get(key));
-        Collections.sort(sortedCounts, Collections.reverseOrder());
-        
-        for(Integer count : sortedCounts){
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
+        List<Integer> sortedCount = new ArrayList<>();
+
+        for(Integer key : map.keySet()) sortedCount.add(map.get(key));
+        Collections.sort(sortedCount, Collections.reverseOrder());
+
+        for(Integer count : sortedCount){
             k -= count;
             answer++;
             if(k<=0) break;
