6fd5a4710d3eaa7e1a2afa87ad47389c128d8c13
vyt153 <92364685+vyt153@users.noreply.github.com>
2023-08-10 15:16:29 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.55 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,18 +3,16 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         for(int i = 0; i<s.length(); i++){
-            String subStr = s.substring(0, i);
-            String str = s.substring(i, s.length())+subStr;
-            
+            String str = s.substring(i, s.length())+s.substring(0, i);
             String[] checkStr = str.split("");
             Stack<String> stack = new Stack<>();
             for(int j = 0; j<checkStr.length; j++){
-                
-                stack.push(checkStr[j]);
-                if((stack.peek().equals(")")&&stack.search("(")==2)||
-                  (stack.peek().equals("}")&&stack.search("{")==2)||
-                  (stack.peek().equals("]")&&stack.search("[")==2)){
-                    stack.pop();stack.pop();
+                if(stack.empty()) stack.push(checkStr[j]);
+                else{
+                    if(checkStr[j].equals(")")&&stack.peek().equals("(")) stack.pop();
+                    else if(checkStr[j].equals("}")&&stack.peek().equals("{")) stack.pop();
+                    else if(checkStr[j].equals("]")&&stack.peek().equals("[")) stack.pop();
+                    else stack.push(checkStr[j]);
                 }
             }
             if(stack.empty()) answer++;
